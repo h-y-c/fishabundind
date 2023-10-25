c2cf1f2debf0364c42849563fccca3c50d69d775
Hsiao-Yun Chang <hsiaoyunchang.hyc@gmail.com>
2023-10-25 11:29:40 -0400

    update plotfishai

--- R/plot.fishai.r
+++ R/plot.fishai.r
@@ -1,18 +1,27 @@
 #' plot.fishai function
-#' Plot annual abundance index
+#' Plot annual abundance index#'
 #'
-#' @param df A dataset contains a variable of year and a variable of abundanc indices.
-#' @param ... further arguments passed to or from other methods.
-#' @param Year A variable of Year of data collection.
-#' @param AbundIndex A variable of annual abundance indices.
+#' @name plot.fishai
+#'
+#' @param df A dataset contains a variable of year and a variable of abundance indices.
+#' @param ... arguments passed to other methods
 #'
 #' @import dplyr
 #' @import ggplot2
 #'
-#' @return This function returns a graph of the annual abundance indices time series.
+#' @export
+plot <- function(df, ...) {
+  UseMethod("plot")
+}
+
+#' @param Year A variable of Year of data collection.
+#' @param AbundIndex A variable of annual abundance indices.
+#'
+#' @return This function returns a graph of the abundance indices time series.
 #'
 #' @rdname plot.fishai
 #' @export
+#'
 #' @examples
 #' \dontrun{
 #'  aiQ<-fishai(dataset=fishQ,Year="yr",
@@ -24,7 +33,7 @@
 #'             VolumeSample="volsamp",
 #'             N_individual="nind",
 #'             SeasonEst=TRUE,first_thr=0.05,durationEst=7)
-#'  plot(aiQ,Year=Year,AbundIndex=AbundIndex)
+#'  plot(aiQ)
 #'
 #'  aiX<-fishai(dataset=fishX,Year="yr",
 #'             Week="wk",
@@ -39,17 +48,6 @@
 #'
 #' }
 #'
-
-# plot.fishai.default<-function(df, ... ,Year=NULL,AbundIndex=NULL){
-#   fishdf<-df%>%dplyr::rename(Year=Year,AbundIndex=AbundIndex)
-#   ggplot(data=fishdf, aes(x=Year, y=AbundIndex, group=1)) +
-#     geom_line()+
-#     geom_point()+
-#     theme_bw()  +
-#     ylab("Abundance Index") +
-#     xlab("Year")
-# }
-
 plot.fishai<-function(df, ... ,Year=NULL,AbundIndex=NULL){
   fishdf<-df%>%dplyr::rename(Year=Year,AbundIndex=AbundIndex)
   ggplot(data=fishdf, aes(x=Year, y=AbundIndex, group=1)) +
@@ -59,4 +57,7 @@ plot.fishai<-function(df, ... ,Year=NULL,AbundIndex=NULL){
     ylab("Abundance Index") +
     xlab("Year")
 }
+
+
+
 
