db369005cf25d1a60a3064e11483f632ab05506b
Hsiao-Yun Chang <hsiaoyunchang.hyc@gmail.com>
2023-10-25 10:11:21 -0400

    update data

--- R/data.r
+++ R/data.r
@@ -1,8 +1,8 @@
 #' Hypothetical fish survey data for species Q
 #'
-#' The data are assumed collected from a fishery-independent survey following 
-#'  a stratified random sampling design with regions within the sampling area and 
-#'  strata within a given region. The survey was conducted over week for each year. 
+#' The data are assumed collected from a fishery-independent survey following
+#'  a stratified random sampling design with regions within the sampling area and
+#'  strata within a given region. The survey was conducted over week for each year.
 #'
 #' @format A data frame with columns:
 #' \describe{
@@ -16,7 +16,9 @@
 #'  \item{volsamp}{Volume of water sampled of a tow.}
 #'  \item{nind}{Number of individuals (fish) sampled of a tow.}
 #'  \item{spe}{Species name.}
-#' 
+#'
+#'  }
+#'
 #' @examples
 #' \dontrun{
 #'  fishQ
